7b9ecedbaa64421609c56fff18d0a01818ea2bd6
Davling-EXE <nadavcohen700@gmail.com>
2023-11-18 22:48:50 +0200

    better organized + 2.6

---
+++ 2.6/Client2.6.py
@@ -0,0 +1,25 @@
+import socket
+
+SERVER_IP = "127.0.0.1"
+PORT = 8820
+MAX_PACKAGE = 1024
+
+
+def main():
+    my_socket = socket.socket()
+
+    try:
+        my_socket.connect((SERVER_IP, PORT))
+        while True:
+            my_socket.send(input("input a command ").encode())
+            print(my_socket.recv(MAX_PACKAGE).decode())
+
+    except socket.error as err:
+        print(err)
+
+    finally:
+        my_socket.close()
+
+
+if __name__ == '__main__':
+    main()
---
+++ 2.6/Server2.6.py
@@ -0,0 +1,47 @@
+"""
+author - nadav
+date   - 03/11/23
+exercise 2.6 server
+"""
+import socket
+import struct
+import datetime
+import random
+
+IP = '0.0.0.0'
+PORT = 8820
+QUEUE_SIZE = 1
+MAX_PACKET = 1024
+
+
+def main():
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    try:
+        server_socket.bind((IP, PORT))
+        server_socket.listen(QUEUE_SIZE)
+        # endless loop to receive client after client
+        while True:
+            print('server is up and running')
+            comm_socket, client_address = server_socket.accept()
+            try:
+                user_input = comm_socket.recv(4).decode()
+                if user_input == "TIME":
+                    comm_socket.send(datetime.datetime.now().strftime("%H:%M:%S").encode())
+                elif user_input == "NAME":
+                    comm_socket.send("Bofadese".encode())
+                elif user_input == "RAND":
+                    comm_socket.send(str(random.randint(1, 10)).encode())
+                elif user_input == "EXIT":
+                    comm_socket.close()
+            except socket.error as msg:
+                print('client socket disconnected- ' + str(msg))
+            finally:
+                comm_socket.close()
+    except socket.error as msg:
+        print('failed to open server socket - ' + str(msg))
+    finally:
+        server_socket.close()
+
+
+if __name__ == '__main__':
+    main()
