52670d377f9787d3229eb32ded2e590cce273a9c
Davling-EXE <nadavcohen700@gmail.com>
2023-11-18 23:04:06 +0200

    Final(?) 2.6 Client

--- 2.6/Client2.6.py
+++ 2.6/Client2.6.py
@@ -5,14 +5,25 @@ PORT = 8820
 MAX_PACKAGE = 1024
 
 
+def valid_command(string):
+    if string == "TIME" or string == "NAME" or string == "RAND" or string == "EXIT":
+        return True
+    else:
+        return False
+
+
 def main():
     my_socket = socket.socket()
 
     try:
         my_socket.connect((SERVER_IP, PORT))
         while True:
-            my_socket.send(input("input a command ").encode())
-            print(my_socket.recv(MAX_PACKAGE).decode())
+            user_input = input("input a command ")
+            if valid_command(user_input):
+                my_socket.send(user_input.encode())
+                print(my_socket.recv(MAX_PACKAGE).decode())
+            else:
+                print("Invalid command")
 
     except socket.error as err:
         print(err)
