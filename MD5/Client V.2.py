e1b090537929a97324bf1a3edb7362246aadf68b
Davling-EXE <nadavcohen700@gmail.com>
2024-10-06 22:39:09 +0300

    MD5 bruteforce (need to do comments)

---
+++ MD5/Client V.2.py
@@ -0,0 +1,78 @@
+import threading
+import socket
+import os
+import hashlib
+
+SERVER_IP = "127.0.0.1"
+PORT = 8820
+
+
+class Client:
+    def __init__(self):
+        self.my_socket = None
+        self.done = False
+        self.solution = None
+        self.threads = None
+        self.to_decode = None
+        self.lock = threading.Lock()
+
+    def check_num(self):
+        """
+        brute forces within a given range
+        :return:
+        """
+        start = int(self.my_socket.recv(10).decode())
+        end = start + 9999
+        start = str(start).zfill(10)
+        end = str(end).zfill(10)
+        cur = start
+        while True:
+            if int(end) < int(cur):
+                return
+            elif self.to_decode == hashlib.md5(cur.encode()).hexdigest():
+                self.done = True
+                self.solution = cur
+                print(cur)
+                return
+            else:
+                cur = int(cur) + 1
+                cur = str(cur).zfill(10)
+
+    def main(self):
+        """
+        the main function that starts the client
+        :return:
+        """
+        try:
+            self.my_socket = socket.socket()
+            self.my_socket.connect((SERVER_IP, PORT))
+            self.to_decode = self.my_socket.recv(32).decode()
+            while True:
+                if self.my_socket.recv(1).decode() == "0":
+                    if not self.done:
+                        self.my_socket.send("cont".encode())
+                        core_count = str(os.cpu_count()).zfill(2)
+                        self.my_socket.send(core_count.encode())
+                        self.threads = []
+                        for i in range(os.cpu_count()):
+                            self.threads.append(threading.Thread(target=self.check_num))
+                            self.threads[i].start()
+                        while threading.active_count() > 4:
+                            pass
+                    else:
+                        self.my_socket.send("done".encode())
+                        self.my_socket.send(self.solution.encode())
+                        self.my_socket.close()
+                        break
+                else:
+                    self.done = True
+                    self.my_socket.close()
+                    break
+
+        except socket.error as err:
+            print(err)
+
+
+if __name__ == '__main__':
+    client = Client()
+    client.main()
---
+++ MD5/Server V.2.py
@@ -0,0 +1,65 @@
+"""
+author - nadav
+date   - 03/11/23
+exercise 2.6 server
+"""
+import socket
+import hashlib
+import threading
+
+"""
+constants
+"""
+
+IP = '0.0.0.0'
+PORT = 8820
+QUEUE_SIZE = 1
+
+
+class Server:
+    def __init__(self):
+        self.server_socket = None
+        self.to_decode = hashlib.md5(b"0003500000").hexdigest()
+        self.cur = "0000000000"
+        self.lock = threading.Lock()
+        self.done = False
+        self.solution = "Hasn't found yet"
+
+    def handle_client(self, client_socket):
+        client_socket.send(self.to_decode.encode())
+        while not self.done:
+            client_socket.send("0".encode())
+            state = client_socket.recv(4).decode()
+            if state == "done":
+                self.solution = client_socket.recv(10).decode()
+                self.done = True
+            elif state == "cont":
+                num_of_cores = int(client_socket.recv(2).decode())
+                for i in range(num_of_cores):
+                    with self.lock:
+                        client_socket.send(self.cur.encode())
+                        self.cur = str(int(self.cur) + 10000).zfill(10)
+        client_socket.send("1".encode())
+
+    def recieve_client(self):
+        while not self.done:
+            client_socket, client_address = self.server_socket.accept()
+            threading.Thread(target=self.handle_client, args=(client_socket,)).start()
+
+    def main(self):
+        self.server_socket = socket.socket()
+        self.server_socket.bind(("0.0.0.0", 8820))
+        self.server_socket.listen()
+        print("Server is up and running")
+        threading.Thread(target=self.recieve_client).start()
+        while True:
+            if not self.solution == "Hasn't found yet":
+                print("the original number is: " + self.solution)
+                self.server_socket.close()
+                break
+        return
+
+
+if __name__ == '__main__':
+    server = Server()
+    server.main()
